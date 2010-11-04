e3c05dbe2153e419960edd46498550b80ec5f302
Zach <zach@puppetlabs.com>
2010-11-04 16:59:30 -0700

    change named.conf layout

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,26 @@
 class dns {
     include dns::params
+    
     $namedconf_path = $dns::params::namedconf_path
-    $dns_dir = $dns::params::dns_dir
+    $dnsdir = $dns::params::dnsdir
     $dns_server_package = $dns::params::dns_server_package
+    $rndckeypath = $dns::params::rndckeypath
+    $rndc_alg = $dns::params::rndc_alg
+    $rndc_secret = $dns::params::rndc_secret
 
-    package { "dns": 
-        ensure => installed,
-        name => "${dns_server_package}";
+    if $operatingsystem != "Darwin" { #linux specifics
+        package { "dns": 
+            ensure => installed,
+            name => "${dns_server_package}";
+        }
+
+    	iptables { 
+            "dns":
+                proto => "udp",
+                dport => "53",
+                jump => "ACCEPT",
+            }
     }
-
-	iptables { 
-        "dns":
-            proto => "udp",
-            dport => "53",
-            jump => "ACCEPT",
-	}
 	
 	file {
 		"$namedconf_path":
@@ -23,15 +29,20 @@ class dns {
 			mode	=> 644,
 			require	=> Package["dns"],
 			content	=> template("dns/named.conf.erb");
-        "$dns_dir":
+        "$dnsdir":
             ensure => directory,
             owner => root,
             group => root,
             mode => 755;
+        "$vardir": 
+            owner => root,
+            group => 0,
+            mode => 755,
+            ensure => directory;
 	}
 
 	include concat::setup
-    concat { "${dns_dir}/named.conf.zones": }
+    concat { "${dnsdir}/named.conf.zones": }
    
     service {
         "named":
@@ -39,5 +50,13 @@ class dns {
             ensure    => "running",
             require   => Package["dns"];
    }
+
+    dns::rndckey {
+        "rndc-key":
+            secret => $rndc_secret,
+            alg    => $rndc_alg;
+    }
+        
+        
 }
 
