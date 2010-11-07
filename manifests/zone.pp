76b31ca9aa1a8eb814599f8f27fc5183803c4b97
Zach Leslie <zach.leslie@gmail.com>
2010-11-07 11:47:41 -0800

    support zonefile creation

--- manifests/zone.pp
+++ manifests/zone.pp
@@ -1,19 +1,30 @@
-define dns::zone ($zonetype="master",$soa,$reverse="false"){
+define dns::zone ($zonetype="master",$soa,$reverse="false",$ttl="10800",$soaip){
+    $contact = "root@${name}"
+    $serial = "2010110702"
     include dns
     include dns::params
 
     $zone = $name
-    $filename = "db.${zone}"
+    $filename = "zones/db.${zone}"
     $dnsdir = $dns::params::dnsdir
     $zonefilename = "${type}/${name}"
     $publicviewpath = $dns::params::publicviewpath
     $zonefilepath = $dns::params::zonefilepath
+    $vardir = $dns::params::vardir
+
+    include concat::setup
 
     concat::fragment {
         "dns_zone_${zone}": # this sets the named zones config
             target => "$publicviewpath",
             content => template("dns/publicView.conf.erb");
+        "dns_zonefile_${zone}": # build zonefile header
+            order => "05",
+            target => "${vardir}/${filename}",
+            content => template("dns/zone.header.erb");
     }
+
+    concat { "${vardir}/${filename}": }
 
 }
 
